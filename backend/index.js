95aeb755a78d4c57d255ffc119e5f6fbf2463092
Saurav sharma <53432918+Saurav-sharma6@users.noreply.github.com>
2025-06-12 20:00:02 +0000

    Adding Frontend and Backend folder

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Import routes
+
+const userRoutes = require('./routes/userRoutes');
+
+
+// Initialize Express app
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://asingh2513:Ig5XYOYcE1TAGz9d@cluster0.kbt2kws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Routes
+
+app.use('/api/users', userRoutes);
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Routes for users
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/:id', userController.getProfile);
+router.put('/:id', userController.updateProfile);
+
+module.exports = router;
