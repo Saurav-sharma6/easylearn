b8493dad5b7c69c8294a99c8c950843239053660
Amrinder <asingh2513@gmail.com>
2025-06-14 03:04:59 +0530

    Adding USER routes models and SignUp functionality

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,36 @@
-
+// server.js
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
-// Import routes
+const userRoutes = require('./routes/userRoutes');
 
-
-
-
-// Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
 app.use(express.json());
-
-// Connect to MongoDB
-mongoose.connect("mongodb+srv://asingh2513:Ig5XYOYcE1TAGz9d@cluster0.kbt2kws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+app.use(helmet());
+app.use(morgan('dev'));
 
 // Routes
+app.use('/api/auth', userRoutes);
 
+// DB Connection
+const PORT = process.env.PORT;
 
-
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  });
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+router.post('/register', authController.register); //register route
+router.post('/login', authController.login); // login route
+
+module.exports = router;
