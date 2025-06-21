dd5bb43d190bb145602da037682a17f5b9f40663
Shreejana Shrestha <stha.shreezana@gmail.com>
2025-06-21 08:52:05 -0400

    minor import fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const {auth, adminMiddleware} = require('../middleware/authMiddleware');
 
 router.post('/register', authController.register); //register route
 router.post('/login', authController.login); // login route
